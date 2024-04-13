3d28d3311f6b1e82e4f17a1453cf6d35e99d3bf4
锦衣 <73991742+ChrisLo0751@users.noreply.github.com>
2024-04-13 10:49:11 +0800

    add gin and zap modules

--- apis/user-web/main.go
+++ apis/user-web/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"g-shopping/user-web/initialize"
+	"go.uber.org/zap"
+)
+
+var (
+	port = 5621
 )
 
 func main() {
-	initialize.Routers()
+	initialize.InitialLogger()
+	r := initialize.Routers()
+
+	zap.S().Infof("启动服务器， 端口：%d", port)
+
+	if err := r.Run(fmt.Sprintf(":%d", port)); err != nil {
+		zap.S().Error("启动服务器失败 ", err.Error())
+	}
 }
